eaec716eb4318b1d3b128b2f17f926ddd4dba49d
Luciotavares <lgtavares@inf.ufpel.edu.br>
2023-08-04 19:54:51 -0300

    att trab2

--- LeetCode/Trabalho1/main.c
+++ LeetCode/Trabalho1/main.c
@@ -1,22 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-char *longestNiceSubstring(char *s) {
-  char chave;
-  int cont = 0;
-  char letra;
-  char *p = NULL;
-  for (int i = 0; i < strlen(s); i++) {
-    chave = s[i];
-    if (chave == s[i]) {
-      cont++;
-    }
-    while (cont > 1) {
-      letra = chave;
-      p = strstr(s, letra); // retorna um ponteiro para o inicio da substring.
-      return p;
-    }
-  }
  
 }
 int main(void) {
